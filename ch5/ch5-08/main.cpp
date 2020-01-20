2638c8902132ba41ea56b347847f0b3a366b8acc
Interactics <zz31@naver.com>
2020-01-20 21:23:10 +0900

    add ch5-08

--- ch5/ch5-08/main.cpp
+++ ch5/ch5-08/main.cpp
@@ -2,9 +2,18 @@
 #include <cstring>
 
 int main() {
-	char str[] = {};
+	char str[100];
 	std::cout << "영어 단어들을 입력하십시오(끝내려면 done을 입력) : ";
 
-	std::cin << 
+	int words = 0;
 
+	do {
+		std::cin >> str;
+		words++;
+	} while (strcmp(str, "done") != 0);
+	
+	words--;
+	std::cout << "총 " << words << "단어가 입력되었습니다." << std::endl;
+	
+	return 0;
 }
