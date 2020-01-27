261716c0d50b5d1680236df7863ae889f5e6bff9
Interactics <zz31@naver.com>
2020-01-27 19:56:43 +0900

    add ch6/ch6-08

---
+++ ch6/ch6-08/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstdlib> //exit함수 사용을 위해
+
+int main() {
+
+	std::string fileName; // 파일명 
+	std::ifstream inFile;
+
+	std::cout << "데이터 파일의 이름을 입력하십시오 : ";
+	std::cin >> fileName;
+
+	inFile.open(fileName);
+
+	if (!inFile.is_open()) {
+		std::cout << fileName << " 파일을 열 수 없습니다. \n" << "파일을 종료합니다. " << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	char ch; // 문자 입력
+	int numChar = 0; //문자의 갯수
+
+	while (!inFile.eof()) {
+		numChar++;
+		inFile >> ch;
+	}
+
+	std::cout << "파일 끝에 도착하였습니다\n" << "파일을 종료합니다." << std::endl;
+	std::cout << "문자의 수(화이트 스페이스 포함) : " << numChar << std::endl;
+
+	return 0;
+}
