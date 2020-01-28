74b2c6d270af927d7d3d6a47e5f52a4900c43613
Interactics <zz31@naver.com>
2020-01-28 20:52:51 +0900

    bug fix ch6-01, ch6-07

--- ch6/ch6-01/main.cpp
+++ ch6/ch6-01/main.cpp
@@ -5,6 +5,10 @@ using namespace std;
 int main() {
 	char ch;
 	while (cin.get(ch) and ch != '@'){
+		if (!isalpha(ch)) {
+			cout << "문자가 아니므로 종료합니다. " << endl;
+			break;
+		}
 		if (ch >= 'a' and ch <= 'z') ch += 'A' - 'a';
 		else if (ch >= 'A' and ch <= 'Z') ch -= 'A' - 'a';
 		cout << ch;
--- ch6/ch6-07/main.cpp
+++ ch6/ch6-07/main.cpp
@@ -2,47 +2,46 @@
 #include <cctype>
 
 int main() {
-	char ch;
+	char ch[10];
 
 	int num_aiueo = 0;
 	int num_anti_aiueo = 0;
 	int num_etc = 0;
 
 	std::cout << "단어들을 입력하시오 (끝내려면 q) : " << std::endl;
-	std::cin.get(ch);
+
 	while (true) {
-		if (isalpha(ch)) {
-			switch (ch) {
+		std::cin >> ch;
+
+		if (isalpha(ch[0])) {
+			switch (ch[0]) {
 			case 'a':
 			case 'i':
 			case 'u':
 			case 'e':
 			case 'o':
+			case 'A':
+			case 'I':
+			case 'U':
+			case 'E':
+			case 'O':
 				num_aiueo++;
 				break;
+
 			default :
 				num_anti_aiueo++;
 				break;
 			}
-			if (ch == 'q') {
-				std::cin.get(ch);
-
-				if (!isalpha(ch)) {
+			if (ch[0] == 'q') {
+				if (!isalpha(ch[1])) {
 					std::cout << "프로그램 종료 " << std::endl;
 					num_anti_aiueo--;
 					break;
 				}
-				else num_anti_aiueo++;
 			}
 
 		}
 		else num_etc++;
-
-		do {
-			std::cin.get(ch);
-		} while (!isspace(ch));
-
-		std::cin.get(ch); //빈칸 리딩 방지.
 	}
 
 	std::cout << "모음으로 시작하는 단어 수 : " << num_aiueo << std::endl;
