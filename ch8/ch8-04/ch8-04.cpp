d1e36a3bdc28e593e61e0de61ac3ae2a0fcb5712
Interactics <zz31@naver.com>
2020-02-04 14:59:53 +0900

    bug fix

--- ch8/ch8-04/ch8-04.cpp
+++ ch8/ch8-04/ch8-04.cpp
@@ -6,6 +6,7 @@ struct stringy {
 	char* str;
 	int ct;
 };
+
 void set(stringy& stgy, const char* ch);
 void show(const stringy& str, int n = 1);
 void show(const char* str, int n = 1);
@@ -14,7 +15,6 @@ int main(void)
 {
 	stringy beauy;
 	char testing[] = "Reality isn't what it used to be.";
-
 	set(beauy, testing);
 	show(beauy);
 	show(beauy, 3);
@@ -23,23 +23,22 @@ int main(void)
 	show(testing);
 	show(testing, 2);
 	show("Done!");
-
+	delete[] beauy.str;
 	return 0;
 }
 
 void set(stringy& stgy, const char* ch) {
 	char* str_store;
-	int strSize = strlen(ch);
-	
-	str_store = new char[strSize];
-
-	strcpy(stgy.str, str_store);
+	const int lenSize = strlen(ch);
+	stgy.str = new char[lenSize + 1];
+	stgy.ct = lenSize;
+	strcpy(stgy.str, ch);
 }
 
 void show(const stringy& str, int n) {
-	for (int i = 0; i < n; i++) cout << str.str << str.ct << endl;
+	for (int i = 0; i < n; i++) cout << "str :" << str.str << "/t ct :" << str.ct << endl;
 }
 
 void show(const char* str, int n) {
-	for (int i = 0; i < n; i++) cout << str<< endl;
+	for (int i = 0; i < n; i++) cout << str << endl;
 }
