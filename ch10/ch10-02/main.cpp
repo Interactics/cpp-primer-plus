d9c8d31a2da40c45fb26a50796cdd40f8f033e37
Interactics <56077549+Interactics@users.noreply.github.com>
2020-02-09 21:39:36 +0900

    Update main.cpp

--- ch10/ch10-02/main.cpp
+++ ch10/ch10-02/main.cpp
@@ -2,15 +2,24 @@
 #include "person.h"
 
 int main() {
-	using std::cout;
-	using std::endl;
+    using std::cout;
+    using std::endl;
 
-	Person one;
-	Person two("Smythecraft");
-	Person three("Dimwiddy", "Sam");
-	one.Show();
-	cout << endl;
-	one.FormalShow();
+    Person one;
+    Person two("Smythecraft");
+    Person three("Dimwiddy", "Sam");
+    
+    one.Show();
+    one.FormalShow();
+    cout << endl;
 
-	return 0;
+    two.Show();
+    two.FormalShow();
+    cout << endl;
+
+    three.Show();
+    three.FormalShow();
+    cout << endl;
+
+    return 0;
 }
