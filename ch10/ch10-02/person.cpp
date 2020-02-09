90fe4cad87c8fff6bf20d1a3003236cee358fb3d
Interactics <56077549+Interactics@users.noreply.github.com>
2020-02-09 21:39:19 +0900

    Update person.cpp

--- ch10/ch10-02/person.cpp
+++ ch10/ch10-02/person.cpp
@@ -1,3 +1,19 @@
 #include "person.h"
+#include <iostream>
 
-Person::Person
+Person::Person(const std::string& ln, const char* fn){
+    lname = ln;
+    strcpy(fname, fn);
+}
+
+
+void Person::Show() const {
+    using namespace std;
+    cout << fname << " " << lname << endl;
+}
+
+void Person::FormalShow() const {
+    using namespace std;
+    cout << lname << " " << fname << endl;
+}
+
